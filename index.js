b0309496ea8102997d102eb64574883d1c049f4c
onem0 <kremersneo0@gmail.com>
2023-11-08 21:37:44 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const axios = require("axios")
+const {exec} = require('child_process');
+const fs = require("fs")
+const prompt = require('prompt-sync')();
+
+let token
+
+axios({
+    method: 'post',
+    url: "http://localhost:2500/connectClient",
+    headers: {},
+    data: {
+        "client": "receiver"
+    }
+}).then((response) => {
+    token = response.data.token
+
+    console.log("Token:", token)
+
+    setInterval(() => {
+        axios({
+            method: "get",
+            url: "http://localhost:2500/checkQueue",
+            headers: {
+                "token": token
+            }
+        }).then((response) => {
+
+            if (response.data.status === 401) {
+
+            } else {
+                // console.log(response.data)
+                // console.log(response.data.length)
+
+                for (let i = 0; i < response.data.length; i++) {
+
+                    if (response.data[i].job === "startApp") {
+
+                        startApp(response.data[i].comment)
+                    }
+                }
+
+                function startApp(appName) {
+
+
+                    const data = JSON.parse(fs.readFileSync('./config.json', {encoding: 'utf8'}));
+
+
+                    for (let i = 0; i < data.paths.length; i++) {
+
+                        if (appName === data.paths[i].name) {
+
+                            exec("cd '" + data.paths[i].path + "'; start " + data.paths[i].exe)
+
+                            console.log("Starting", data.paths[i].exe)
+
+                        } else {
+                            console.log(false)
+
+                            let addApp = {
+                                "name": appName,
+                                "path": "",
+                                "exe": ""
+                            }
+
+                            const path = prompt('Path > ');
+                            const exe = prompt('exe name > ');
+
+                            addApp.path = path
+                            addApp.exe = exe
+
+                            data.paths.push(addApp)
+
+                            fs.writeFileSync("./config.json", JSON.stringify(data))
+                        }
+                    }
+
+
+                }
+            }
+
+        }).catch((error) => {
+            console.log(error)
+        })
+    }, 1000)
+
+
+});
+
